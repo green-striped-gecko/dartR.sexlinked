cdeebfe2e975256d2fdd3a0b3cce1270e2f1076a
unknown <nanisrobledo@gmail.com>
2026-03-19 16:19:20 +1100

    Corrected typo

--- R/gl.drop.sexlinked.r
+++ R/gl.drop.sexlinked.r
@@ -20,7 +20,7 @@
 #'
 #' @references
 #' \itemize{
-#' \item Robledo‐Ruiz, D. A., Austin, L., Amos, J. N., Castrejón‐Figueroa, J.,
+#' \item Robledo‐Ruiz, D. A., Austin, L., Amos, J. N., Castrejon‐Figueroa, J.,
 #'  Harley, D. K., Magrath, M. J., Sunnucks, P., & Pavlova, A. (2023). 
 #'  Easy‐to‐use R functions to separate reduced‐representation genomic datasets
 #'   into sex‐linked and autosomal loci, and conduct sex assignment. Molecular 
